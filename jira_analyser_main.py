c2f3d5fd8190b26d7f469dda42ce122e848e7807
Ruiyao-wu <81689305+Ruiyao-wu@users.noreply.github.com>
2021-09-06 11:48:41 +0200

    Update jira_analyser_main.py

--- jira_analyser_main.py
+++ jira_analyser_main.py
@@ -12,31 +12,6 @@ def main():
     #*download JIRA_Report from jira filter to the assigned file path
     driver= DriverBuilder(download_path)
     driver.download_from_friver(url)
-    #* copy the daily download data to Jira_Export sheet and return WB wkbAnalysis and a dataframe of the download value
-    app = NewAPP(download_path)
-    wkbAnalysis, DFJiraExp = app.copy_sheet()
-    #*get sheet 
-    shtReleaseMatch = wkbAnalysis.sheets['Release_Matching']
-    shtVars = wkbAnalysis.sheets['Variables']
-    shtToday = wkbAnalysis.sheets['Today']
-    shtData = wkbAnalysis.sheets['Data']
-    #shtNoDeadline = wkbAnalysis.sheets['Ticket_noDeadline']
-    #* update Data of WB wkbAnalysis
-    print('------updating the data of Jira_Analyser-------')
-    Var = VariablesDatabase(shtVars)
-    Var.Update_Variables(DFJiraExp)
-    # Ticket_Non = NoDeadlineDatabase(shtNoDeadline)
-    # Ticket_Non.Update_TicketnoDeadline(DFJiraExp)
-    # Ticket_Non.Update_RealeasenoDeadline()
-    Rematch = ReleaseDatabase(shtReleaseMatch)
-    Rematch.Update_Rematch(DFJiraExp)
-    Today = TodayDatabase(shtToday)
-    Today.Update_Today(Var.sht)
-    Data = DataDatabase(shtData)
-    Data.Update_Database(Today.sht)
-    #*close the app
-    app.close_app(wkbAnalysis)
-
 
 if __name__ == "__main__":
     main()
